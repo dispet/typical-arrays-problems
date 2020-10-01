3cc44a1673d722026b0a87aee428f48f0339e425
dispet <62215289+dispet@users.noreply.github.com>
2020-10-01 15:22:22 +0300

    fix: update index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+
+exports.min = function min (array) {
+    if (!Array.isArray(array) || array.length === 0 )
+  return 0;
+    else {
+        return array.sort((a, b) => a - b)[0];
+    }
+}
+
+exports.max = function max (array) {
+    if (!Array.isArray(array) || array.length === 0 )
+        return 0;
+    else {
+        return array.sort((a, b) => b - a)[0];
+    }
+}
+
+exports.avg = function avg (array) {
+    if (!Array.isArray(array) || array.length === 0 )
+        return 0;
+    else {
+        return array.reduce((a,b) => a + b ) / array.length;
+    }
+}
